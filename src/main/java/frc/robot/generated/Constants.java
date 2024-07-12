9c15ea876a00aa28eae42be0e06f9e7e3d221316
amarforce <amarvk@gmail.com>
2024-07-12 09:46:17 -0700

    Update and rename Constants.java to test2.java

--- src/main/java/frc/robot/generated/Constants.java
+++
@@ -1,8 +0,0 @@
-public final class Constants {
-    public static final class ConfigFiles {
-        public static final String DRIVETRAIN_PID = "DrivetrainPIDConfig.json";
-        public static final String ARM_PID = "ArmPIDConfig.json";
-        public static final String INTAKE_PID = "IntakePIDConfig.json";
-        // ... other config file names
-    }
-}
