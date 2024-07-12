230b3406f2b946c024d138771005eb64b63f14ab
amarforce <amarvk@gmail.com>
2024-07-12 09:36:33 -0700

    Update Intake.java

--- src/main/java/frc/robot/Subsystems/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//* New Config Code**//
+/* New Config Code */
 import frc.robot.config.ConfigManager;
 
 public class Intake extends SubsystemBase {
