b21993e6709a334336c729d3682cf36e6b78d40b
amarforce <amarvk@gmail.com>
2024-07-12 09:43:56 -0700

    Create Constants.java

---
+++ src/main/java/frc/robot/generated/Constants.java
@@ -0,0 +1,8 @@
+public final class Constants {
+    public static final class ConfigFiles {
+        public static final String DRIVETRAIN_PID = "DrivetrainPIDConfig.json";
+        public static final String ARM_PID = "ArmPIDConfig.json";
+        public static final String INTAKE_PID = "IntakePIDConfig.json";
+        // ... other config file names
+    }
+}
